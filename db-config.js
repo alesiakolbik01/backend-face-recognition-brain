ca987c3912604c10f98e528b2524b1a3c25223ce
Alesia Kolbik <alesyakolbik@gmail.com>
2023-08-08 12:19:36 +0200

    delete file

--- db-config.js
+++
@@ -1,8 +0,0 @@
-export const dbConfig = {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Request-Headers': '*',
-      'api-key': 'K5flkjOfBLRgo45Jlq8HVHCQ4Wwefc89AZi6h00NdztJSLrSveq2zpbtP5QAa8If',
-    }
-};
